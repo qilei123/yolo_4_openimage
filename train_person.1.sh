9e4a6caca07230dc842f859257abcc12b34b4492
qilei123 <qileimail123@gmail.com>
2018-06-15 00:45:03 -0400

    restart train person

--- train_person.1.sh
+++ train_person.1.sh
@@ -1,1 +1,1 @@
-./darknet detector train cfg/openimgv4_person1.data cfg/yolov3-openimgv4_person1.cfg backup_person1/yolov3-openimgv4_person1.backup -gpus 0
+./darknet detector train cfg/openimgv4_person1.data cfg/yolov3-openimgv4_person1.cfg darknet53.conv.74 -gpus 0
